8e6212b20666d55dd810b36d5ecdbd3d68d0e6df
varcheta <tatiana.vs@inbox.ru>
2021-10-15 11:46:53 +0300

    Убраны ненужные промежуточные результаты

--- Lesson01Homework/Task02.py
+++ Lesson01Homework/Task02.py
@@ -1,10 +1,10 @@
 time_seconds = int(input('Введите время в секундах: '))
 
 hours = time_seconds // 3600
-print(hours)
+# print(hours)
 minutes = (time_seconds % 3600)//60
-print(minutes)
+# print(minutes)
 seconds = time_seconds - hours * 3600 - minutes * 60
-print(seconds)
+# print(seconds)
 
 print("{:>2}ч:{:>2}м:{:>2}с".format(hours, minutes, seconds))
